e3d99a4c3b379edd5589365f67a2f99a1f1ac739
spookyle4n <103913003+spookyle4n@users.noreply.github.com>
2022-10-27 08:01:18 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,33 @@
+from random import seed
+from random import randint
+import time
+import random
+import string
+print('''
+\033[34m 
+  ____      ____
+ |    |    |    |________________________________    
+ |    |    |    |   __    |     __    |    ___   \
+ |    | /\ |    |  |  |   |    |  \___|   |   |   |
+ |    |/  \|    |  |__|   |    |      |   |___|   |
+  \             /         |    |      |           /
+   \___________/__________|____|      |__________/
+   
+               tiktok jahsehrare
+               insta: spookyle4n
+
+''')
+a = input("[?] Generate wordlist? [y/n]: ")
+if a == 'y':
+    for i in range(10000000):
+        randomnumber = chr(random.randint(ord('0'), ord('9')))
+        randomnumber2 = chr(random.randint(ord('0'), ord('9')))
+        randomnumber3 = chr(random.randint(ord('0'), ord('9')))
+        randomnumber4 = chr(random.randint(ord('0'), ord('9')))
+        randomnumber5 = chr(random.randint(ord('0'), ord('9')))
+        randomnumber6 = chr(random.randint(ord('0'), ord('9')))
+        randomUpperLetter = chr(random.randint(ord('A'), ord('Z')))
+        randomLowerLetter = chr(random.randint(ord('a'), ord('z')))
+
+        print(randomnumber + randomnumber2 + randomnumber3 + randomnumber4 + randomnumber5 + randomnumber6 + randomUpperLetter + randomLowerLetter, flush=True)
+        time.sleep(0.00001)
